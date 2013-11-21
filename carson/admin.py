0cb93276b725c0aca3c12a04c83028d73b24c4b6
Les Smithson <lsmithso@hare.demon.co.uk>
2013-11-21 10:11:00 +0000

    add activate/deactivate to Account + Tag admin

--- carson/admin.py
+++ carson/admin.py
@@ -3,13 +3,32 @@ from carson.models import Account, Tag
 from carson.utils import lookup_twitter_ids
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ["twitter_username", "twitter_id"]
-    actions = ['populate_twitter_ids']
+    list_display = ["twitter_username", "twitter_id", 'active']
+    actions = ('populate_twitter_ids', 'activate', 'deactivate')
 
     def populate_twitter_ids(self, request, queryset):
         updated = lookup_twitter_ids(queryset)
         self.message_user(request, "%d account(s) updated" % updated)
     populate_twitter_ids.short_description = "Lookup Twitter IDs"
 
+    def activate(self, request, queryset):
+	queryset.update(active = True)
+
+    def deactivate(self, request, queryset):
+	queryset.update(active = False)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", 'active']
+    actions = ('activate', 'deactivate')
+
+    def activate(self, request, queryset):
+	queryset.update(active = True)
+
+    def deactivate(self, request, queryset):
+	queryset.update(active = False)
+
+    
+
 admin.site.register(Account, AccountAdmin)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
